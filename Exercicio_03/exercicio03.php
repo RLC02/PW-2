f5c94b2de128feea5fe91e515567e98f5bd879ca
RLC02 <99838772+RLC02@users.noreply.github.com>
2023-02-13 16:24:15 -0300

    exc

---
+++ Exercicio_03/exercicio03.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+
+        $var1 = 1; 
+        $varX = 9.50; 
+        $varY = 9.50; 
+        $var2 = $varX + $varY; 
+        $varSTR = "Atribui String"; 
+        $varletra = "a"; 
+        $varbool = true; 
+
+        echo "<strong> Exemplo de uma aplicação em PHP </strong>";
+        print "<P>";
+        echo "<strong> Conteúdo de var1: </strong> ";
+        echo $var1;
+        print "<P>";
+        echo "<strong> Conteúdo de varX: </strong>";
+        echo $varX; 
+        print "<P>";
+
+        echo "<strong> Conteúdo de varY: </strong>";
+        echo $varY; 
+        print "<P>";
+        echo "<strong> Conteúdo de var2: </strong>";
+        echo $var2; 
+        print "<P>";
+
+        echo "<strong> Conteúdo de varSTR: </strong>";
+        echo $varSTR;
+        print "<P>";
+
+        echo "<strong> Conteúdo de varletra: </strong>";
+        echo $varletra;
+        print "<P>";
+        echo "<strong> Conteúdo de varbool: </strong>";
+        echo $varbool
+
+    ?>
+
+</body>
+</html> 
