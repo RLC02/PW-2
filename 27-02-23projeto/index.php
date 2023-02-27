94459390f7975d4ae3e603e4638ee2f2587b6089
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2023-02-27 16:17:35 -0300

    Add files via upload

---
+++ 27-02-23projeto/index.php
@@ -0,0 +1,23 @@
+<?php
+ 
+ public class Pessoa(){
+    private $nome;
+    private $endereco;
+    private $bairro;
+    private $cep;
+    private $cidade;
+    private $estado;
+
+    //Metodo get
+    public function getNome(){
+        return $this->nome;
+    }
+    //Metodo set
+    public function setNome({
+        
+    }
+
+
+ }
+
+?>
